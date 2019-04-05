e9c7b07b89928882af00b6471390fab8cbc7331a
JINSAN NOH <zingoworks@gmail.com>
2019-04-05 13:55:18 +0900

    Update domain enum classes

---
+++ src/main/java/com/zingoworks/blackjack/domain/HandType.java
@@ -0,0 +1,7 @@
+package com.zingoworks.blackjack.domain;
+
+public enum HandType {
+    BLACKJACK,
+    BURST,
+    NORMAL;
+}
---
+++ src/main/java/com/zingoworks/blackjack/domain/ResultType.java
@@ -0,0 +1,7 @@
+package com.zingoworks.blackjack.domain;
+
+public enum ResultType {
+    WINNER,
+    LOSER,
+    TIE;
+}
