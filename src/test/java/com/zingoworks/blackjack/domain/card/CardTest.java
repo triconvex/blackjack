b87a442ebd6cf2a8c650244a3236eaf38f2c561e
JINSAN NOH <zingoworks@gmail.com>
2019-04-09 20:06:43 +0900

    Implement Card test cases

--- src/test/java/com/zingoworks/blackjack/domain/card/CardTest.java
+++ src/test/java/com/zingoworks/blackjack/domain/card/CardTest.java
@@ -10,8 +10,18 @@ public class CardTest {
     public static final Card FIVE_CLUB = Card.of(CLUB, 5);
     public static final Card TEN_CLUB = Card.of(CLUB, 10);
 
+    @Test(expected = IllegalArgumentException.class)
+    public void verifyInputTest() {
+        Card.of(CLUB, 14);
+    }
+
     @Test
     public void isAceTest() {
         assertThat(ACE_CLUB.isAce()).isTrue();
+    }
+
+    @Test
+    public void verifyConvertingTest() {
+        assertThat(Card.of(CLUB, 11).getNumber()).isEqualTo(10);
     }
 }
