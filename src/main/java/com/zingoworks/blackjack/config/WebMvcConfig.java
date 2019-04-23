6b26298ecf642a094f2fe9aa64c521e797e0fb13
JINSAN NOH <zingoworks@gmail.com>
2019-04-23 21:47:01 +0900

    Remove HomeController and add viewController configurations

--- src/main/java/com/zingoworks/blackjack/config/WebMvcConfig.java
+++ src/main/java/com/zingoworks/blackjack/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.zingoworks.blackjack.security.LoginUserHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,6 +20,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("").setViewName("index");
     }
 
 }
